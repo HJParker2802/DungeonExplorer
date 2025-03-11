929ef2a3723066458a30cf12b5e68710ca7b22ca
HJParker2802 <125221273+HJParker2802@users.noreply.github.com>
2025-03-11 14:35:09 +0000

    Altered the getters and setters
    Altered the getters and setters to ensure that the input data will always be valid

--- DungeonExplorer-master/Player.cs
+++ DungeonExplorer-master/Player.cs
@@ -10,8 +10,44 @@ namespace DungeonExplorer
 
 
 
-        public string Name { get; set; }
-        public int Health { get; set; }
+        public string Name 
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("Name has been set to Greg House due to lack of answer");
+                    _name = "Greg House";
+                }
+
+                else
+                {
+                    _name = value;
+                }
+            }
+        }
+        public int Health
+        {
+            get { return _health; }
+            set
+            {
+                if (string.IsNullOrEmpty(value.ToString()))
+                {
+                    Console.WriteLine("Health has been set to 100 due to lack of answer");
+                    _health = 100;
+                }
+                else if (value <= 0)
+                {
+                    Console.WriteLine("Health has been set to 100 due to answer being less than or equal to 0");
+                    _health = 100;
+                }
+                else
+                {
+                    _health = value;
+                }
+            }
+        }
 
         private List<string> inventory = new List<string> { };
         
